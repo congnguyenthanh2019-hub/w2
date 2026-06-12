15b31dde2edbc8655ca46860b5fbd5b623b4d800
congnguyenthanh2019-hub <congnguyenthanh2019@gmail.com>
2026-06-12 23:36:28 +0530

    Add files via upload

--- text_tel.js
+++ text_tel.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-855-520-3858';
-window.defaultText = 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apples Support +1-855-520-3858 to Freeze it!';
+window.defaultNumber = '+1-844-950-5120';
+window.defaultText = 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Applẹ Support +1-844-950-5120 to Freeze it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'gotporn.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'anysex.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'sex.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 unlock it!',
-    'bravotube.net': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'mylust.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'manporn.xxx': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'anybunny.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'txxx.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-855-520-3858 to unlock it!'
+    'xhamster.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'gotporn.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'anysex.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'sex.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 unlock it!',
+    'bravotube.net': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'mylust.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'anybunny.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'txxx.com': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Applẹ ID was recently used at APPLẸ STORE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5120 to unlock it!'
 };
